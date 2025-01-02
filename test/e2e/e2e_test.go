1959736626ebce68b2d3edbce0538392a83c2b66
李志朋 <lizhipeng.629@bytedance.com>
2025-01-02 20:47:12 +0800

    open sidecar

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/magicsong/kidecar/test/utils"
 )
 
-const namespace = "kidecar-system"
+const namespace = "sidecar-system"
 
 var _ = Describe("controller", Ordered, func() {
 	BeforeAll(func() {
@@ -60,7 +60,7 @@ var _ = Describe("controller", Ordered, func() {
 			var err error
 
 			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/kidecar:v0.0.1"
+			var projectimage = "example.com/sidecar:v0.0.1"
 
 			By("building the manager(Operator) image")
 			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
